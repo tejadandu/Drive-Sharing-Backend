72e5357b525a81f751455902e8f17e1ca5c59e21
Teja Dandu <teja.dandu484@gmail.com>
2024-06-17 18:48:21 +0530

    Added .env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
+const { PORT } = require('./config/serverConfig');
+
 const app = express();
 
 
 app.listen(3000, () => {
-    console.log("Server for drive-sharing is Up");
+    console.log(`Server for drive-sharing is Up ${3000}`);
 });
