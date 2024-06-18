9bb61ac8fbbe974d767449f18e845da9d8e0b0d8
Teja Dandu <teja.dandu484@gmail.com>
2024-06-18 10:14:20 +0530

    Added Database setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
-const { PORT } = require('./config/serverConfig');
+const { PORT } = require('./config/server_config');
+const db = require('./config/db_config');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, () => {
+app.get('/api/v1/get', (request, response) => {
+    return response.json({message: "Alive"});
+});
+
+app.listen(PORT, async () => {
     console.log(`Server for drive-sharing is Up ${3000}`);
+    console.log('DB Connected');
 });
